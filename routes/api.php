662696f921f5d062c89c01e9151c2ca08e635cd2
Caio Albuquerque <caio.albuquerque@infobase.com.br>
2022-10-26 01:21:55 -0300

    Finished migrations

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', [AuthController::class, 'createUser']);
+Route::post('login', [AuthController::class, 'loginUser']);
+Route::get('results', function (){
+    return 'User authenticated';
+})->middleware('auth:sanctum');
+
+// Use middleware('auth:sanctum') para as rotas de consulta e cadastro que precisam estar logado
